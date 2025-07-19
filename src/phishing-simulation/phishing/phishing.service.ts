8ce055a5f5d930eb289572c8fecdaa7738273ce4
avihooCohenNice <Avihoo.Cohen@nice.com>
2025-07-19 20:26:40 +0300

    Cymulate-BE

--- src/phishing-simulation/phishing/phishing.service.ts
+++ src/phishing-simulation/phishing/phishing.service.ts
@@ -41,20 +41,13 @@ async sendTestEmail(to: string) {
     text: 'This is a fake phishing email for test.',
   });
 
-  await this.model.findByIdAndUpdate(
-    attemptId,
-    {
-      email: to,
-      content: emailContent,
-      status: 'sent',
-      updatedAt: new Date(),
-    },
-    { new: true }
-  );
-
-  // Debugging output
-  const all = await this.model.find().exec();
-  console.log('All Attempts:', all);
+  await this.model.create({
+    email: to,
+    clicked: false,
+    content: emailContent,
+    status: 'sent',
+    updatedAt: new Date(),
+  });
   console.log('Preview Email Content:\n', info.message.toString());
 }
 
